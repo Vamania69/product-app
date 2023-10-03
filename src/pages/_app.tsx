3287dbd29a68de828f83f2b71e42f1e65907c3a9
varun agrawal <90302777+Vamania69@users.noreply.github.com>
2023-10-03 11:35:51 +0530

    made the app styling in global.css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,8 @@ import { extendTheme } from "@chakra-ui/react";
 import { customChakraTheme } from "../styles/customChakraTheme";
 import { cardTheme } from "../styles/components/CardStyle";
 import { buttonTheme } from "../styles/components/ButtonStyles";
+// import styles from "./styles.module.css";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   const theme = extendTheme(
@@ -13,7 +15,7 @@ export default function App({ Component, pageProps }) {
     { components: { Button: buttonTheme } }
   );
   return (
-    <div className="bg-dark-primary">
+    <div  className="bg_primary">
       <ChakraProvider theme={theme}>
         <Navbar />
         <Component {...pageProps} />
